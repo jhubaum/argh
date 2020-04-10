d978456bcf36826b355ea243fc297faa8167074c
Johannes Huwald <=>
2020-04-10 15:07:43 +0200

    stripped down design to a button to quit and a timer to visit youtube; began implementation

--- index.js
+++ index.js
@@ -1,58 +1,73 @@
-function createOverlay() {
+function createOverlayIndexElement() {
     let d = document.createElement('div');
     d.setAttribute('id', 'argh-container');
-    d.innerHTML =
-        `<form id="argh-form">
-         You just opened a Youtube Video. Make a note why you want to watch it.
-           <select id="argh-reason">
-             <option value="music">To listen to music</option>
-             <option value="hannibal">To kill some time</option>
-             <option value="procrastination">I don't want to work on my next task</option>
-             <option value="break">I want to take a break</option>
-             <option value="research">I am doing some research</option>
-             <option value="friend">Someone send me a video and I want to watch it</option>
-           </select>
-           <input type="submit" value="Continue to Video">
-         </form>`;
 
-    d.classList.add('form-overlay');
+    document.getElementById('container').append(d);
 
     return d;
 }
 
-function openWebsite(url) {
-    window.location.replace(url);
-}
-
-function closeOverlay() {
+function deleteOverlay() {
     let overlay = document.getElementById('argh-container');
     overlay.parentNode.removeChild(overlay);
 }
 
-function formSubmitFunction(reason) {
-    switch(reason) {
-    case "music":
-    case "friend":
-    case "research":
-    case "break":
-        return closeOverlay();
-    case "hannibal":
-        return openWebsite("https://www.youtube.com/watch?v=Ow0lr63y4Mw");
-    case "procrastination":
-        return openWebsite(browser.extension.getURL("resources/procrastination.html"));
-    }
-
+function createActionLink(text, action) {
+    let a = document.createElement('button');
+    a.classList.add('link');
+    a.innerHTML = text;
+    a.addEventListener('click', action);
+    return a;
 }
 
-const WHITELIST_VIDEOS = [
-    "https://www.youtube.com/watch?v=Ow0lr63y4Mw",
-];
-
-if (!WHITELIST_VIDEOS.includes(window.location.href)) {
-    document.getElementById('container').append(createOverlay());
-
-    document.getElementById('argh-form').addEventListener('submit', e => {
-        e.preventDefault();
-        formSubmitFunction(document.getElementById('argh-reason').value);
-    });
+function fillParagraph(p, textBefore, linkText, textAfter, action) {
+    p.innerHTML = textBefore;
+    p.appendChild(createActionLink(linkText, action));
+    p.append(textAfter);
 }
+
+function indulgeTimer(length=15) {
+    // parent element
+    let p = document.createElement('p');
+
+    let startTimer = () => {
+        let remainingTime = length;
+        p.innerHTML = `Wait ${remainingTime} seconds to indulge`;
+        let timer = setInterval(() => {
+            remainingTime -= 1;
+            p.innerHTML = `Wait ${remainingTime} seconds to indulge`;
+            if (remainingTime === 0) {
+                clearInterval(timer);
+                p.innerHTML = '';
+                fillParagraph(p,
+                    `You don't really want to indulge, do you?
+                     Think about how often you have regretted this decision.
+                     If you want to do it despite everything, click `, 'here', '.',
+                              deleteOverlay);
+            }
+        }, 1000);
+        window.addEventListener('blur', () => {
+            if (remainingTime <= 0)
+                return;
+
+            clearInterval(timer);
+            fillParagraph(p, 'You left the tab. Click ', 'here', ' to restart timer. Add nevermind, I changed my mind', startTimer);
+        });
+    };
+    p.appendChild(createActionLink("Indulge anwayws", startTimer));
+    return p;
+}
+
+
+function createContent() {
+    let parent = createOverlayIndexElement();
+    let b = document.createElement('button')
+    b.addEventListener('click', () => window.location.replace('about:blank'));
+    b.innerHTML = "Improve Now";
+
+    parent.append(b);
+
+    parent.append(indulgeTimer());
+}
+
+createContent();
